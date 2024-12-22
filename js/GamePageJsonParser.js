2d4e5b1b43faea9b06a8d281b5410a9ab086af07
AtomTechnologies-cmd <Adam.Developer0@outlook.com>
2024-12-21 22:18:55 -0800

    starting on game page

--- js/GamePageJsonParser.js
+++ js/GamePageJsonParser.js
@@ -18,3 +18,21 @@ function SetCard(id, json){
     let title = card.getElementsByTagName("h5")[0].innerHTML = json.title;
     let description = card.getElementsByTagName("p")[0].innerHTML = json.description.short
 }
+/*
+                <div class="card" style="width: 18rem;">
+                  <img class="card-img-top" src="/dat/img/TenSecondEscape.png" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">10 Second Escape</h5>
+                    <p class="card-text">A fast-paced parkour platformer, beat a level in 10 seconds</p>
+                  </div>
+                  <ul class="list-group list-group-flush justify-content-center align-items-center text-center">
+                    <li class="list-group-item">          
+                      <button type="button" class="btn text-bg-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">More About This Game <span class="bi-arrow-right-square"></span></button>
+                    </li>
+                  </ul>
+                  <div class="card-body justify-content-center align-items-center text-center">
+                    <a href="https://www.youtube.com/watch?v=f38x0_mFzWQ" class="btn text-bg-primary"><span class="bi-youtube"></span> Youtube</a>
+                    <a href="https://apps.microsoft.com/detail/9P807NC9Q6J0" class="btn text-bg-primary"><span class="bi-download"></span> Store Link</a>
+                  </div>
+                </div>
+*/
